4f406831b3247572b0bdf958c7a5a9673207f7be
FPorucznik <porucznik199@wp.pl>
2024-02-06 14:39:56 +0100

    added base project files and set up environment variables

---
+++ api.py
@@ -0,0 +1,7 @@
+from dotenv import load_dotenv
+
+load_dotenv()
+
+
+def get_comments_from_video(url: str):
+    pass
---
+++ app.py
@@ -0,0 +1,9 @@
+from api import get_comments_from_video
+
+
+def app():
+    pass
+
+
+if __name__ == '__main__':
+    app()
